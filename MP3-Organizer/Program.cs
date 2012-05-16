398928373f8c3328ed82145f49e2b3d206275057
Berry Ligtermoet <dev@krypt.nl>
2012-05-16 20:44:02 +0200

    Added project skeleton

---
+++ MP3-Organizer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MP3_Organizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
